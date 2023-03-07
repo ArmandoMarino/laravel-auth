b01b2cb31bce9c0bf76e604d52dd792085b6cbac
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-07 18:40:59 +0100

    user model crypt password

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // MUTATOR crypt passowrd
+    protected function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
