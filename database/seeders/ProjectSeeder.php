dbb2655b4bc7f41e095dc1f83aa79dfba8d8efdc
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-07 18:08:58 +0100

    seeder with faker and migration

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use illuminate\support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            $project = new Project();
+
+            $project->title = $faker->text(20);
+            $project->description = $faker->paragraphs(15, true);
+            $project->image = $faker->imageUrl(250, 250);
+            $project->slug = Str::slug($project->title, '-');
+            $project->save();
+        }
     }
 }
