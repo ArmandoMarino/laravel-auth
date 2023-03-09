77679403acc7677d3f0c6886ba2e6a43e05c98b7
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-09 17:08:26 +0100

    remove faker for images

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -20,7 +20,7 @@ class ProjectSeeder extends Seeder
 
             $project->title = $faker->text(20);
             $project->description = $faker->paragraphs(15, true);
-            $project->image = $faker->imageUrl(250, 250);
+            // $project->image = $faker->imageUrl(250, 250);
             $project->slug = Str::slug($project->title, '-');
             $project->save();
         }
